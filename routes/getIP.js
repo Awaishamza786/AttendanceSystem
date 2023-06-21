292f0b60b9f5cde35e0206fb7d4cf159621ff758
awaishamza786 <awaishamza579@gmail.com>
2023-06-21 10:43:38 +0500

    add ip checker

--- routes/getIP.js
+++ routes/getIP.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const app = express();
+const router = require("express").Router();
 
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   const routerIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   res.send(`Router's IP address: ${routerIP}`);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+module.exports=router
