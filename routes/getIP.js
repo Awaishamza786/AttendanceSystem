10c20e05e7e5465f8ca454a84032200eb88fc7fd
awaishamza786 <awaishamza579@gmail.com>
2023-06-21 11:33:15 +0500

    add ip checker

--- routes/getIP.js
+++ routes/getIP.js
@@ -1,12 +1,38 @@
 const router = require("express").Router();
 
-var requestIp = require('request-ip');
+var requestIp = require("request-ip");
 
+router.get("/", (req, res) => {
+  const routerIP =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  var requestIp = req.connection.remoteAddress;
 
-router.get('/', (req, res) => {
-  const routerIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  var requestIp = req.connection.remoteAddress
-  res.send(`Router's IP address: ${routerIP}`+`var ${requestIp} = requestIp.getClientIp(req);`);
+  const conRemoteAddress = req.connection?.remoteAddress;
+  // req.socket is said to replace req.connection
+  const sockRemoteAddress = req.socket?.remoteAddress;
+  // some platforms use x-real-ip
+  const xRealIP = req.headers["x-real-ip"];
+  // most proxies use x-forwarded-for
+  const xForwardedForIP = (() => {
+    const xForwardedFor = req.headers["x-forwarded-for"];
+    if (xForwardedFor) {
+      // The x-forwarded-for header can contain a comma-separated list of
+      // IP's. Further, some are comma separated with spaces, so whitespace is trimmed.
+      const ips = xForwardedFor.split(",").map((ip) => ip.trim());
+      return ips[0];
+    }
+  })();
+  // prefer x-forwarded-for and fallback to the others
+
+  res.send(
+    `Router's IP address: ${routerIP}` +
+      `||
+    var ${requestIp} = requestIp.getClientIp(req); ||
+    xForwardedForIP ${xForwardedForIP} ||
+     xRealIP ${xRealIP} ||
+      sockRemoteAddress ${sockRemoteAddress} ||
+       conRemoteAddress  ${conRemoteAddress}`
+  );
 });
 
-module.exports=router
+module.exports = router;
